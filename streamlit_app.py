fb5a61bfafb2516caf799e74eafeaeb71e372c9b
gdwsnowflake <104367525+gdwsnowflake@users.noreply.github.com>
2022-04-25 15:41:43 +0200

    Update streamlit_app.py
    added content

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title('My Mom\'s New Healty Diner')
+streamlit.title('My Mom\'s New Healthy Diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
